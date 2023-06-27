cdac5daf29c998b4552e25a1c99d52339e2fff03
Sandeep Kandukuri <sandeep.kandukuri@prutech.local>
2023-06-27 14:38:52 +0530

    done

--- server-script.sh
+++ server-script.sh
@@ -1,6 +1,14 @@
 sudo amazon-linux-extras install java-openjdk11 -y
 sudo yum install git -y
 sudo yum install maven -y
-git clone https://github.com/sandeep9985/addressbook.git
-cd /home/ec2-user/addressbook
+if [ -d "addressbook"]
+then
+  echo "repo is cloned and created"
+  cd /home/ec2-user/addressbook
+  git pull origin b1
+else
+  echo "new repo is creating"
+  git clone https://github.com/sandeep9985/addressbook.git
+  cd /home/ec2-user/addressbook
+fi
 mvn test
